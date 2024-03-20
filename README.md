82de08a91a6a1df5d897c273e5a3247b6afb71eb
Youssef Najjarine <youssefnajjarine@hotmail.com>
2024-03-20 08:02:14 -0500

    added demo.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # week-planner
 
 A dynamic HTML, CSS, and JavaScript week planner app.
+
+![weekly-planner-demo](https://github.com/Youssef-Najjarine/week-planner/assets/71291742/6af9ce30-3a01-4dd2-8507-e5768c957a6d.gif)
--- main.js
+++ main.js
@@ -40,9 +40,8 @@ const localData = localStorage.getItem('data');
 if (localData !== null) {
   data = JSON.parse(localData);
 }
-
+const submit = document.querySelector('.submit');
 const addEntry = document.querySelector('.addEntry');
-// const submit = document.querySelector('.submit');
 const mondayButton = document.querySelector('.monday');
 const tuesdayButton = document.querySelector('.tuesday');
 const wednesdayButton = document.querySelector('.wednesday');
@@ -67,9 +66,15 @@ thursdayButton.addEventListener('click', handleThursdayButton);
 fridayButton.addEventListener('click', handleFridayButton);
 saturdayButton.addEventListener('click', handleSaturdayButton);
 sundayButton.addEventListener('click', handleSundayButton);
+tableBody.addEventListener('click', handleUpdateButton);
 
 function lightUp() {
+  document.querySelector('.whiteBoxAddEntry').textContent = 'Add Entry';
   document.querySelector('.blackBox').classList.add('active');
+}
+
+function saveData() {
+  localStorage.setItem('data', JSON.stringify(data));
 }
 
 function defaultEvent() {
@@ -79,11 +84,43 @@ function defaultEvent() {
     tableBody.appendChild(createEntries(data.monday.mondayEvents[i]));
   }
 }
+
+function createEntries(entry) {
+  const tr = document.createElement('tr');
+  const td1 = document.createElement('td');
+  const td2 = document.createElement('td');
+  const div1 = document.createElement('div');
+  const div2 = document.createElement('div');
+  const p = document.createElement('p');
+  const button1 = document.createElement('button');
+  const button2 = document.createElement('button');
+  tr.appendChild(td1);
+  td1.textContent = entry.time;
+  tr.appendChild(td2);
+  td2.appendChild(div1);
+  div1.setAttribute('class', 'secondTd');
+  div1.appendChild(p);
+  p.setAttribute('class', 'secondTdP');
+  p.textContent = entry.description;
+  div1.appendChild(div2);
+  div2.setAttribute('class', 'updateAndDelete');
+  div2.appendChild(button1);
+  button1.setAttribute('class', 'update');
+  button1.setAttribute('data-entry-id', entry.EntryId);
+  button1.setAttribute('dayOfWeek', entry.dayOfWeek);
+  button1.textContent = 'Update';
+  div2.appendChild(button2);
+  button2.setAttribute('class', 'delete');
+  button2.textContent = 'Delete';
+  return tr;
+}
+
 function handleSubmit(event) {
   event.preventDefault();
   document.querySelector('.blackBox').classList.remove('active');
   if (days.value === 'Monday') {
     const newObject = {};
+    newObject.dayOfWeek = days.value;
     newObject.time = time.value;
     newObject.description = description.value;
     newObject.EntryId = data.monday.nextEntryId;
@@ -91,8 +128,8 @@ function handleSubmit(event) {
     data.monday.nextEntryId++;
     form.reset();
     saveData();
-    // const dataEntryId = event.target.getAttribute('data-entry-id');
-    // data.monday.mondayEvents.splice(data.monday.mondayEvents.length - dataEntryId, 1);
+    const dataEntryId = submit.getAttribute('data-entry-id');
+    data.monday.mondayEvents.splice(data.monday.mondayEvents.length - dataEntryId, 1);
     tableBody.innerHTML = '';
     h2.textContent = 'Scheduled Events for Monday';
     for (let i = 0; i < data.monday.mondayEvents.length; i++) {
@@ -100,6 +137,7 @@ function handleSubmit(event) {
     }
   } else if (days.value === 'Tuesday') {
     const newObject = {};
+    newObject.dayOfWeek = days.value;
     newObject.time = time.value;
     newObject.description = description.value;
     newObject.EntryId = data.tuesday.nextEntryId;
@@ -107,8 +145,8 @@ function handleSubmit(event) {
     data.tuesday.nextEntryId++;
     form.reset();
     saveData();
-    // const dataEntryId = event.target.getAttribute('data-entry-id');
-    // data.tuesday.tuesdayEvents.splice(data.tuesday.tuesdayEvents.length - dataEntryId, 1);
+    const dataEntryId = submit.getAttribute('data-entry-id');
+    data.tuesday.tuesdayEvents.splice(data.tuesday.tuesdayEvents.length - dataEntryId, 1);
     tableBody.innerHTML = '';
     h2.textContent = 'Scheduled Events for Tuesday';
     for (let i = 0; i < data.tuesday.tuesdayEvents.length; i++) {
@@ -116,6 +154,7 @@ function handleSubmit(event) {
     }
   } else if (days.value === 'Wednesday') {
     const newObject = {};
+    newObject.dayOfWeek = days.value;
     newObject.time = time.value;
     newObject.description = description.value;
     newObject.EntryId = data.wednesday.nextEntryId;
@@ -123,8 +162,8 @@ function handleSubmit(event) {
     data.wednesday.nextEntryId++;
     form.reset();
     saveData();
-    // const dataEntryId = event.target.getAttribute('data-entry-id');
-    // data.wednesday.wednesdayEvents.splice(data.wednesday.wednesdayEvents.length - dataEntryId, 1);
+    const dataEntryId = submit.getAttribute('data-entry-id');
+    data.wednesday.wednesdayEvents.splice(data.wednesday.wednesdayEvents.length - dataEntryId, 1);
     tableBody.innerHTML = '';
     h2.textContent = 'Scheduled Events for Wednesday';
     for (let i = 0; i < data.wednesday.wednesdayEvents.length; i++) {
@@ -132,6 +171,7 @@ function handleSubmit(event) {
     }
   } else if (days.value === 'Thursday') {
     const newObject = {};
+    newObject.dayOfWeek = days.value;
     newObject.time = time.value;
     newObject.description = description.value;
     newObject.EntryId = data.thursday.nextEntryId;
@@ -139,8 +179,8 @@ function handleSubmit(event) {
     data.thursday.nextEntryId++;
     form.reset();
     saveData();
-    // const dataEntryId = event.target.getAttribute('data-entry-id');
-    // data.thursday.thursdayEvents.splice(data.thursday.thursdayEvents.length - dataEntryId, 1);
+    const dataEntryId = submit.getAttribute('data-entry-id');
+    data.thursday.thursdayEvents.splice(data.thursday.thursdayEvents.length - dataEntryId, 1);
     tableBody.innerHTML = '';
     h2.textContent = 'Scheduled Events for Thursday';
     for (let i = 0; i < data.thursday.thursdayEvents.length; i++) {
@@ -148,6 +188,7 @@ function handleSubmit(event) {
     }
   } else if (days.value === 'Friday') {
     const newObject = {};
+    newObject.dayOfWeek = days.value;
     newObject.time = time.value;
     newObject.description = description.value;
     newObject.EntryId = data.friday.nextEntryId;
@@ -155,8 +196,8 @@ function handleSubmit(event) {
     data.friday.nextEntryId++;
     form.reset();
     saveData();
-    // const dataEntryId = event.target.getAttribute('data-entry-id');
-    // data.friday.fridayEvents.splice(data.friday.fridayEvents.length - dataEntryId, 1);
+    const dataEntryId = submit.getAttribute('data-entry-id');
+    data.friday.fridayEvents.splice(data.friday.fridayEvents.length - dataEntryId, 1);
     tableBody.innerHTML = '';
     h2.textContent = 'Scheduled Events for Friday';
     for (let i = 0; i < data.friday.fridayEvents.length; i++) {
@@ -164,6 +205,7 @@ function handleSubmit(event) {
     }
   } else if (days.value === 'Saturday') {
     const newObject = {};
+    newObject.dayOfWeek = days.value;
     newObject.time = time.value;
     newObject.description = description.value;
     newObject.EntryId = data.saturday.nextEntryId;
@@ -171,8 +213,8 @@ function handleSubmit(event) {
     data.saturday.nextEntryId++;
     form.reset();
     saveData();
-    // const dataEntryId = event.target.getAttribute('data-entry-id');
-    // data.saturday.saturdayEvents.splice(data.saturday.saturdayEvents.length - dataEntryId, 1);
+    const dataEntryId = submit.getAttribute('data-entry-id');
+    data.saturday.saturdayEvents.splice(data.saturday.saturdayEvents.length - dataEntryId, 1);
     tableBody.innerHTML = '';
     h2.textContent = 'Scheduled Events for Saturday';
     for (let i = 0; i < data.saturday.saturdayEvents.length; i++) {
@@ -180,6 +222,7 @@ function handleSubmit(event) {
     }
   } else if (days.value === 'Sunday') {
     const newObject = {};
+    newObject.dayOfWeek = days.value;
     newObject.time = time.value;
     newObject.description = description.value;
     newObject.EntryId = data.sunday.nextEntryId;
@@ -187,8 +230,8 @@ function handleSubmit(event) {
     data.sunday.nextEntryId++;
     form.reset();
     saveData();
-    // const dataEntryId = event.target.getAttribute('data-entry-id');
-    // data.sunday.sundayEvents.splice(data.sunday.sundayEvents.length - dataEntryId, 1);
+    const dataEntryId = submit.getAttribute('data-entry-id');
+    data.sunday.sundayEvents.splice(data.sunday.sundayEvents.length - dataEntryId, 1);
     tableBody.innerHTML = '';
     h2.textContent = 'Scheduled Events for Sunday';
     for (let i = 0; i < data.sunday.sundayEvents.length; i++) {
@@ -197,40 +240,56 @@ function handleSubmit(event) {
   }
 }
 
-function saveData() {
-  localStorage.setItem('data', JSON.stringify(data));
-}
-
-function createEntries(entry) {
-  const tr = document.createElement('tr');
-  const td1 = document.createElement('td');
-  const td2 = document.createElement('td');
-  const div1 = document.createElement('div');
-  const div2 = document.createElement('div');
-  const button1 = document.createElement('button');
-  const button2 = document.createElement('button');
-  tr.appendChild(td1);
-  td1.textContent = entry.time;
-  tr.appendChild(td2);
-  td2.appendChild(div1);
-  div1.setAttribute('class', 'secondTd');
-  td2.textContent = entry.description;
-  div1.appendChild(div2);
-  div2.setAttribute('class', 'updateAndDelete');
-  div2.appendChild(button1);
-  button1.setAttribute('class', 'update');
-  div2.appendChild(button2);
-  button2.setAttribute('class', 'delete');
-
-  // const tr = document.createElement('tr');
-  // const td1 = document.createElement('td');
-  // td1.textContent = entry.time;
-  // tr.appendChild(td1);
-  // const td2 = document.createElement('td');
-  // td2.textContent = entry.description;
-  // tr.appendChild(td2);
-
-  return tr;
+function handleUpdateButton(event) {
+  form.reset();
+  const upDtBtn = event.target;
+  document.querySelector('.whiteBoxAddEntry').textContent = 'Edit Entry';
+  document.querySelector('.blackBox').classList.add('active');
+  const dayOfWeek = upDtBtn.getAttribute('dayOfWeek');
+  const dataEntryId = upDtBtn.getAttribute('data-entry-id');
+  if (dayOfWeek === 'Sunday') {
+    data.monday.editing = event.target.closest('tr');
+    days.value = data.sunday.sundayEvents[data.sunday.sundayEvents.length - dataEntryId].dayOfWeek;
+    time.value = data.sunday.sundayEvents[data.sunday.sundayEvents.length - dataEntryId].time;
+    description.value = data.sunday.sundayEvents[data.sunday.sundayEvents.length - dataEntryId].description;
+    submit.setAttribute('data-entry-id', dataEntryId);
+  } else if (dayOfWeek === 'Monday') {
+    data.monday.editing = event.target.closest('tr');
+    days.value = data.monday.mondayEvents[data.monday.mondayEvents.length - dataEntryId].dayOfWeek;
+    time.value = data.monday.mondayEvents[data.monday.mondayEvents.length - dataEntryId].time;
+    description.value = data.monday.mondayEvents[data.monday.mondayEvents.length - dataEntryId].description;
+    submit.setAttribute('data-entry-id', dataEntryId);
+  } else if (dayOfWeek === 'Tuesday') {
+    data.tuesday.editing = event.target.closest('tr');
+    days.value = data.tuesday.tuesdayEvents[data.tuesday.tuesdayEvents.length - dataEntryId].dayOfWeek;
+    time.value = data.tuesday.tuesdayEvents[data.tuesday.tuesdayEvents.length - dataEntryId].time;
+    description.value = data.tuesday.tuesdayEvents[data.tuesday.tuesdayEvents.length - dataEntryId].description;
+    submit.setAttribute('data-entry-id', dataEntryId);
+  } else if (dayOfWeek === 'Wednesday') {
+    data.tuesday.editing = event.target.closest('tr');
+    days.value = data.wednesday.wednesdayEvents[data.wednesday.wednesdayEvents.length - dataEntryId].dayOfWeek;
+    time.value = data.wednesday.wednesdayEvents[data.wednesday.wednesdayEvents.length - dataEntryId].time;
+    description.value = data.wednesday.wednesdayEvents[data.wednesday.wednesdayEvents.length - dataEntryId].description;
+    submit.setAttribute('data-entry-id', dataEntryId);
+  } else if (dayOfWeek === 'Thursday') {
+    data.thursday.editing = event.target.closest('tr');
+    days.value = data.thursday.thursdayEvents[data.thursday.thursdayEvents.length - dataEntryId].dayOfWeek;
+    time.value = data.thursday.thursdayEvents[data.thursday.thursdayEvents.length - dataEntryId].time;
+    description.value = data.thursday.thursdayEvents[data.thursday.thursdayEvents.length - dataEntryId].description;
+    submit.setAttribute('data-entry-id', dataEntryId);
+  } else if (dayOfWeek === 'Friday') {
+    data.thursday.editing = event.target.closest('tr');
+    days.value = data.friday.fridayEvents[data.friday.fridayEvents.length - dataEntryId].dayOfWeek;
+    time.value = data.friday.fridayEvents[data.friday.fridayEvents.length - dataEntryId].time;
+    description.value = data.friday.fridayEvents[data.friday.fridayEvents.length - dataEntryId].description;
+    submit.setAttribute('data-entry-id', dataEntryId);
+  } else if (dayOfWeek === 'Saturday') {
+    data.thursday.editing = event.target.closest('tr');
+    days.value = data.saturday.saturdayEvents[data.saturday.saturdayEvents.length - dataEntryId].dayOfWeek;
+    time.value = data.saturday.saturdayEvents[data.saturday.saturdayEvents.length - dataEntryId].time;
+    description.value = data.saturday.saturdayEvents[data.saturday.saturdayEvents.length - dataEntryId].description;
+    submit.setAttribute('data-entry-id', dataEntryId);
+  }
 }
 
 function handleMondayButton() {
@@ -250,7 +309,6 @@ function handleTuesdayButton() {
 }
 
 function handleWednesdayButton() {
-
   tableBody.innerHTML = '';
   h2.textContent = 'Scheduled Events for Wednesday';
   for (let i = 0; i < data.wednesday.wednesdayEvents.length; i++) {
@@ -289,5 +347,3 @@ function handleSundayButton() {
     tableBody.appendChild(createEntries(data.sunday.sundayEvents[i]));
   }
 }
-
-createEntries();
